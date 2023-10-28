d6ec1691cecd3ed251bb88ffae89407d6ff0303b
sudoarun <arunrajorya1999@gmail.com>
2023-10-28 16:28:07 +0530

    added basic style to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,17 @@ export default function Home() {
   return (
     <div className="container-fluid vh-100 w-100 d-flex justify-content-center align-items-center">
       <div className="text-center">
-        <h1>Text craft</h1>
+        <h1 className="fw-bold">Text Craft</h1>
         <span>A Simple Way to Share Text Online!</span>
         <div>
-          <input placeholder="Enter Secret Name" />
+          <div className="border border-dark mt-3">
+            <span className="bg-black text-white py-2 px-3">TextCraft /</span>
+            <input
+              placeholder="Enter Secret Name"
+              type="text"
+              className="border-0 input-focus-none py-1"
+            />
+          </div>
         </div>
       </div>
     </div>
