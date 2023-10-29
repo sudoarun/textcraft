0e4c788b6663500a53c00411c786fa7798d2bd27
sudoarun <arunrajorya1999@gmail.com>
2023-10-29 12:08:58 +0530

    added msg notification

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -2,12 +2,21 @@ import { onValue, ref, set, update } from "firebase/database";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { database } from "../firebase/config";
+import { message } from "antd";
 
 const UserPage = () => {
   const params = useParams();
   const { name } = params;
   const [data, setData] = useState("");
+  const [notification, contextApi] = message.useMessage();
   const db = database;
+  const sendMsg = (data) => {
+    const { msg, type } = data;
+    notification.open({
+      type: type,
+      content: msg,
+    });
+  };
 
   const getDataBase = () => {
     onValue(ref(db, "textCraft/" + name), (snapshot) => {
@@ -15,7 +24,18 @@ const UserPage = () => {
       if (resData === null) {
         set(ref(db, "textCraft/" + name), {
           sharedText: "",
+        }).catch(() => {
+          const data = {
+            type: "error",
+            msg: "Something Wrong!!",
+          };
+          sendMsg(data);
         });
+        const data = {
+          type: "success",
+          msg: "File Created!",
+        };
+        sendMsg(data);
         return;
       }
       setData(resData?.sharedText);
@@ -25,9 +45,21 @@ const UserPage = () => {
     const value = e.target.value;
     setData(value);
     setTimeout(() => {
-      update(ref(db, "textCraft/" + name), {
+      const res = update(ref(db, "textCraft/" + name), {
         sharedText: value,
       });
+      const data = {
+        type: "success",
+        msg: "Data updated!",
+      };
+      res.catch(() => {
+        const data = {
+          type: "error",
+          msg: "Something Wrong!!",
+        };
+        sendMsg(data);
+      });
+      sendMsg(data);
     }, 2000);
   };
 
@@ -36,13 +68,16 @@ const UserPage = () => {
   }, []);
 
   return (
-    <textarea
-      value={data}
-      onChange={onFormChange}
-      placeholder="Type Your Text Here ..."
-      className="w-100 border-0 input-focus-none"
-      style={{ height: "98vh" }}
-    />
+    <>
+      {contextApi}
+      <textarea
+        value={data}
+        onChange={onFormChange}
+        placeholder="Type Your Text Here ..."
+        className="w-100 border-0 input-focus-none"
+        style={{ height: "98vh" }}
+      />
+    </>
   );
 };
 
